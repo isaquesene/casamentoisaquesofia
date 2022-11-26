661e37e5f749dcb7d14b4c91d6ccd5d43a03190c
IsaqueSene <109972304+isaquesene@users.noreply.github.com>
2022-11-26 19:08:55 -0300

    Add files via upload

---
+++ decora.js
@@ -0,0 +1,7 @@
+function decora(){
+	alert("Item decorativo a sua escolha!");
+}
+
+function localiza(){
+	alert("Av. Pres. Tancredo Neves, 1331 - Jardim Americano, São José dos Campos - SP, 12225-011 Ap31 Bloco 2");
+}
